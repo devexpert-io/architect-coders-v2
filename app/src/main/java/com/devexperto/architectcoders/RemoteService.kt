6354713f2180185254efc5b48a63d8ddca7aa996
antoniolg <antonio@devexperto.com>
2022-01-27 16:34:12 +0100

    Configure Retrofit and test it with a first request

---
+++ app/src/main/java/com/devexperto/architectcoders/RemoteService.kt
@@ -0,0 +1,11 @@
+package com.devexperto.architectcoders
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RemoteService {
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMovies(@Query("api_key") apiKey: String): RemoteResult
+
+}
