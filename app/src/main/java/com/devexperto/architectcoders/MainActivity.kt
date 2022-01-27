83bc82e08a31d0167e3684a9d3dc712357bdfa23
antoniolg <antonio@devexperto.com>
2022-01-27 16:51:48 +0100

    Show the title results in an adapter

--- app/src/main/java/com/devexperto/architectcoders/MainActivity.kt
+++ app/src/main/java/com/devexperto/architectcoders/MainActivity.kt
@@ -3,16 +3,23 @@ package com.devexperto.architectcoders
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.devexperto.architectcoders.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = MoviesAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter = adapter
 
         lifecycleScope.launch {
             val movies = RemoteConnection.service.listPopularMovies(getString(R.string.api_key))
-            toast(movies.results.size.toString())
+            adapter.movies = movies.results
         }
     }
 }
