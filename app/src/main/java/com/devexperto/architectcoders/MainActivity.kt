7cd7f4027bde5fdaa2e51e587f3c963d0ce34c24
antoniolg <antonio@devexperto.com>
2022-01-27 16:34:12 +0100

    Configure Retrofit and test it with a first request

--- app/src/main/java/com/devexperto/architectcoders/MainActivity.kt
+++ app/src/main/java/com/devexperto/architectcoders/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.devexperto.architectcoders
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            val movies = RemoteConnection.service.listPopularMovies(getString(R.string.api_key))
+            toast(movies.results.size.toString())
+        }
     }
 }
