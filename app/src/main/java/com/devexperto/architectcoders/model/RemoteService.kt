05b4e305170689028c8aca3cd8bbf42593d7d185
antoniolg <antonio@devexperto.com>
2022-01-28 10:36:07 +0100

    Find region and use it in the request

--- app/src/main/java/com/devexperto/architectcoders/model/RemoteService.kt
+++ app/src/main/java/com/devexperto/architectcoders/model/RemoteService.kt
@@ -6,6 +6,9 @@ import retrofit2.http.Query
 interface RemoteService {
 
     @GET("discover/movie?sort_by=popularity.desc")
-    suspend fun listPopularMovies(@Query("api_key") apiKey: String): RemoteResult
+    suspend fun listPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): RemoteResult
 
 }
