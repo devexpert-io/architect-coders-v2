a7dc8b56b5e2f3128d4503b5db3dd41a1e7da712
antoniolg <antonio@devexperto.com>
2022-02-03 12:10:31 +0100

    Use ListAdapter instead of basic one for movies

--- app/src/main/java/com/devexperto/architectcoders/ui/MoviesAdapter.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/MoviesAdapter.kt
@@ -2,28 +2,22 @@ package com.devexperto.architectcoders.ui
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.devexperto.architectcoders.R
 import com.devexperto.architectcoders.databinding.ViewMovieBinding
 import com.devexperto.architectcoders.model.Movie
 
 class MoviesAdapter(private val listener: (Movie) -> Unit) :
-    RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
-
-    var movies: List<Movie> by basicDiffUtil(
-        emptyList(),
-        areItemsTheSame = { old, new -> old.id == new.id }
-    )
+    ListAdapter<Movie, MoviesAdapter.ViewHolder>(basicDiffUtil { old, new -> old.id == new.id }) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = parent.inflate(R.layout.view_movie, false)
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int = movies.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val movie = movies[position]
+        val movie = getItem(position)
         holder.bind(movie)
         holder.itemView.setOnClickListener { listener(movie) }
     }
--- app/src/main/java/com/devexperto/architectcoders/ui/extensions.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/extensions.kt
@@ -6,9 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlin.properties.Delegates
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
     LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
@@ -17,21 +15,13 @@ fun ImageView.loadUrl(url: String) {
     Glide.with(context).load(url).into(this)
 }
 
-inline fun <VH : RecyclerView.ViewHolder, T> RecyclerView.Adapter<VH>.basicDiffUtil(
-    initialValue: List<T>,
+inline fun <T> basicDiffUtil(
     crossinline areItemsTheSame: (T, T) -> Boolean = { old, new -> old == new },
     crossinline areContentsTheSame: (T, T) -> Boolean = { old, new -> old == new }
-) =
-    Delegates.observable(initialValue) { _, old, new ->
-        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                areItemsTheSame(old[oldItemPosition], new[newItemPosition])
+) = object : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean =
+        areItemsTheSame(oldItem, newItem)
 
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                areContentsTheSame(old[oldItemPosition], new[newItemPosition])
-
-            override fun getOldListSize(): Int = old.size
-
-            override fun getNewListSize(): Int = new.size
-        }).dispatchUpdatesTo(this@basicDiffUtil)
-    }
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
+        areContentsTheSame(oldItem, newItem)
+}
