f4cff19bd199e54ecccaa68c14dbef1fcb9a33b6
antoniolg <antonio@devexperto.com>
2022-02-03 12:17:57 +0100

    Extract detailed info from DetailActivity to a custom View

---
+++ app/src/main/java/com/devexperto/architectcoders/ui/MovieDetailInfoView.kt
@@ -0,0 +1,35 @@
+package com.devexperto.architectcoders.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.devexperto.architectcoders.model.Movie
+
+class MovieDetailInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    fun setMovie(movie: Movie) = with(movie) {
+        text = buildSpannedString {
+
+            bold { append("Original language: ") }
+            appendLine(originalLanguage)
+
+            bold { append("Original title: ") }
+            appendLine(originalTitle)
+
+            bold { append("Release date: ") }
+            appendLine(releaseDate)
+
+            bold { append("Popularity: ") }
+            appendLine(popularity.toString())
+
+            bold { append("Vote Average: ") }
+            append(voteAverage.toString())
+        }
+    }
+}
