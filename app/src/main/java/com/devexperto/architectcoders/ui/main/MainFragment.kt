e73f19a21514266c788381bc2713cb66f19d3fbf
antoniolg <antonio@devexperto.com>
2022-02-11 16:59:21 +0100

    Use StateHolder for MainFragment

--- app/src/main/java/com/devexperto/architectcoders/ui/main/MainFragment.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/main/MainFragment.kt
@@ -1,20 +1,14 @@
 package com.devexperto.architectcoders.ui.main
 
-import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import com.devexperto.architectcoders.R
 import com.devexperto.architectcoders.databinding.FragmentMainBinding
-import com.devexperto.architectcoders.model.Movie
 import com.devexperto.architectcoders.model.MoviesRepository
-import com.devexperto.architectcoders.ui.common.PermissionRequester
 import com.devexperto.architectcoders.ui.common.launchAndCollect
 import com.devexperto.architectcoders.ui.common.visible
-import kotlinx.coroutines.launch
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -22,42 +16,28 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         MainViewModelFactory(MoviesRepository(requireActivity().application))
     }
 
-    private val adapter = MoviesAdapter { viewModel.onMovieClicked(it) }
+    private lateinit var mainState : MainState
 
-    private val coarsePermissionRequester = PermissionRequester(
-        this,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
+    private val adapter = MoviesAdapter { mainState.onMovieClicked(it) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mainState = buildMainState()
+
         val binding = FragmentMainBinding.bind(view).apply {
             recycler.adapter = adapter
         }
 
         viewLifecycleOwner.launchAndCollect(viewModel.state) { binding.updateUI(it) }
+
+        mainState.requestLocationPermission {
+            viewModel.onUiReady()
+        }
     }
 
     private fun FragmentMainBinding.updateUI(state: MainViewModel.UiState) {
         progress.visible = state.loading
         state.movies?.let(adapter::submitList)
-        state.navigateTo?.let(::navigateTo)
-
-        if (state.requestLocationPermission) {
-            requestLocationPermission()
-        }
-    }
-
-    private fun navigateTo(movie: Movie) {
-        val action = MainFragmentDirections.actionMainToDetail(movie)
-        findNavController().navigate(action)
-        viewModel.onNavigateDone()
-    }
-
-    private fun requestLocationPermission() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            coarsePermissionRequester.request()
-            viewModel.onLocationPermissionChecked()
-        }
     }
 }
