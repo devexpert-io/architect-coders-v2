d5a257207e9e9402955b5342cb98e4ceb7ce39ba
antoniolg <antonio@devexperto.com>
2022-03-17 16:29:05 +0100

    Dagger: Scopes for Fragments

--- app/src/main/java/com/devexperto/architectcoders/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/detail/DetailFragment.kt
@@ -14,7 +14,14 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private val safeArgs: DetailFragmentArgs by navArgs()
 
-    private val viewModel: DetailViewModel by viewModels { app.component.detailViewModelFactory }
+    private lateinit var component: DetailFragmentComponent
+
+    private val viewModel: DetailViewModel by viewModels { component.detailViewModelFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        component = app.component.plus(DetailFragmentModule(safeArgs.movieId))
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
