5b465570aa92423d961a9418bfc18f350de1fd6f
antoniolg <antonio@devexperto.com>
2022-03-17 16:29:05 +0100

    Dagger: Scopes for Fragments

--- app/src/main/java/com/devexperto/architectcoders/di/AppComponent.kt
+++ app/src/main/java/com/devexperto/architectcoders/di/AppComponent.kt
@@ -1,18 +1,20 @@
 package com.devexperto.architectcoders.di
 
 import android.app.Application
-import com.devexperto.architectcoders.ui.detail.DetailViewModelFactory
-import com.devexperto.architectcoders.ui.main.MainViewModelFactory
+import com.devexperto.architectcoders.ui.detail.DetailFragmentComponent
+import com.devexperto.architectcoders.ui.detail.DetailFragmentModule
+import com.devexperto.architectcoders.ui.main.MainFragmentComponent
+import com.devexperto.architectcoders.ui.main.MainFragmentModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class, DataModule::class, UseCaseModule::class, ViewModelsModule::class])
+@Component(modules = [AppModule::class, DataModule::class, UseCaseModule::class])
 interface AppComponent {
 
-    val mainViewModelFactory: MainViewModelFactory
-    val detailViewModelFactory: DetailViewModelFactory
+    fun plus(module: MainFragmentModule): MainFragmentComponent
+    fun plus(module: DetailFragmentModule): DetailFragmentComponent
 
     @Component.Factory
     interface Factory {
--- app/src/main/java/com/devexperto/architectcoders/ui/main/MainFragment.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/main/MainFragment.kt
@@ -11,11 +11,18 @@ import com.devexperto.architectcoders.ui.common.launchAndCollect
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    private val viewModel: MainViewModel by viewModels { app.component.mainViewModelFactory }
+    private lateinit var component: MainFragmentComponent
+
+    private val viewModel: MainViewModel by viewModels { component.mainViewModelFactory }
 
     private lateinit var mainState: MainState
 
     private val adapter = MoviesAdapter { mainState.onMovieClicked(it) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        component = app.component.plus(MainFragmentModule())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
