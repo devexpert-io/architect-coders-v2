3cb03633410f6d543bd0c3a2283020dc82616d2f
antoniolg <antonio@devexperto.com>
2023-06-06 16:17:55 +0200

    Find region and use it in the request

--- app/src/main/java/com/devexperto/architectcoders/ui/MainActivity.kt
+++ app/src/main/java/com/devexperto/architectcoders/ui/MainActivity.kt
@@ -1,27 +1,94 @@
 package com.devexperto.architectcoders.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.FloatRange
+import androidx.annotation.IntRange
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.devexperto.architectcoders.R
 import com.devexperto.architectcoders.databinding.ActivityMainBinding
 import com.devexperto.architectcoders.model.RemoteConnection
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            lifecycleScope.launch {
+                val location = getLocation(isGranted)
+                val movies = RemoteConnection.service.listPopularMovies(
+                    getString(R.string.api_key),
+                    getRegionFromLocation(location)
+                )
+                adapter.movies = movies.results
+            }
+        }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val adapter = MoviesAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.recycler.adapter = adapter
 
-        lifecycleScope.launch {
-            val movies = RemoteConnection.service.listPopularMovies(getString(R.string.api_key))
-            adapter.movies = movies.results
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    private suspend fun getLocation(granted: Boolean): Location? {
+        return if (granted) findLastLocation() else null
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result)
+                }
+        }
+
+    private suspend fun getRegionFromLocation(location: Location?): String {
+        val geocoder = Geocoder(this@MainActivity)
+        val fromLocation = location?.let {
+            geocoder.getFromLocationCompat(
+                location.latitude,
+                location.longitude,
+                1,
+
+                )
+        }
+        return fromLocation?.firstOrNull()?.countryCode ?: "US"
+    }
+
+    @Suppress("DEPRECATION")
+    private suspend fun Geocoder.getFromLocationCompat(
+        @FloatRange(from = -90.0, to = 90.0) latitude: Double,
+        @FloatRange(from = -180.0, to = 180.0) longitude: Double,
+        @IntRange maxResults: Int
+    ): List<Address> = suspendCancellableCoroutine { continuation ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getFromLocation(latitude, longitude, maxResults) {
+                continuation.resume(it)
+            }
+        } else {
+            continuation.resume(getFromLocation(latitude, longitude, maxResults) ?: emptyList())
         }
     }
 }
